d1d4e7a4d0a37b2b6c5a239d372784e7601b3f3a
mamun4447 <abdullah15-12557@diu.edu.bd>
2022-10-11 17:19:41 +0600

    Blogs

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 
 const Blogs = () => {
-  return <div>Blogs</div>;
+  return (
+    <div className="container w-[50%] mx-auto rounded-md mt-44">
+      <div className="collapse rounded-t-md border">
+        <input type="checkbox" className="peer" />
+        <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          What is the purpose of React router?
+        </div>
+        <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          <p>
+            React Router is a standard library for routing in React. It enables
+            the navigation among views of various components in a React
+            Application, allows changing the browser URL, and keeps the UI in
+            sync with the URL
+          </p>
+        </div>
+      </div>
+      <div className="collapse border">
+        <input type="checkbox" className="peer" />
+        <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          How does context API work?
+        </div>
+        <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          <p>
+            The React Context API is a way for a React app to effectively
+            produce global variables that can be passed around. This is the
+            alternative to "prop drilling" or moving props from grandparent to
+            child to parent, and so on. Context is also touted as an easier,
+            lighter approach to state management using Redux
+          </p>
+        </div>
+      </div>
+      <div className="collapse rounded-b-md border">
+        <input type="checkbox" className="peer" />
+        <div className="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          What is UseRef hooks?
+        </div>
+        <div className="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          <p>
+            The useRef Hook allows you to persist values between renders. It can
+            be used to store a mutable value that does not cause a re-render
+            when updated. It can be used to access a DOM element directly.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -11,8 +11,7 @@ const Question = ({ questions }) => {
   return (
     <div className="container  mx-auto p-5 mb-6 shadow-xl border rounded-2xl">
       <p onClick={notify} className=" text-sm text-right cursor-pointer">
-        <FontAwesomeIcon icon="fa-regular fa-eye" />
-        View
+        <FontAwesomeIcon icon="fa-duotone fa-eye" /> Answer
       </p>
       <h2 className="text-2xl w-[75%] mx-auto">{question}</h2>
 
