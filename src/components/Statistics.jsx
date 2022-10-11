a5d564c9fb960d733801f41482a978c8bb517718
mamun4447 <abdullah15-12557@diu.edu.bd>
2022-10-11 13:23:38 +0600

    Statistics

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,10 +1,21 @@
 import React, { useContext } from "react";
+import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts";
 import { DataContext } from "./Main";
 
 const Statistics = () => {
   const data = useContext(DataContext);
   // console.log(data);
-  return <div>Statistics{data.length}</div>;
+  return (
+    <div className="mt-10 ml-64">
+      <BarChart width={600} height={300} data={data}>
+        <XAxis dataKey="name" stroke="#8884d8" />
+        <YAxis dataKey="total" stroke="#8884d8" />
+        <Tooltip />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <Bar dataKey="total" fill="#8884d8" barSize={30} />
+      </BarChart>
+    </div>
+  );
 };
 
 export default Statistics;
